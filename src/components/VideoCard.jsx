496655029cb156f637b7e035e5e92428e7e6ef84
revBilliCici <style.loopy@gmail.com>
2023-02-01 01:30:32 +0500

    fixed netlify  issue

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -27,7 +27,7 @@ const VideoCard = ({
     >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
-          image={snippet?.thumbnails?.high?.url}
+          image={snippet?.thumbnails?.high?.url || demoThumbnailUrl}
           alt={snippet?.title}
           sx={{ height: 180, width: { xs: "100%", sm: "358px", md: "320px" } }}
         />
